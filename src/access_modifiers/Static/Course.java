fc3bef59c6f0f3ebf0ba37ef69f86c45d088a437
fatihademtas0 <fatihademtas@outlook.com>
2024-04-07 00:11:26 +0300

    Static

---
+++ src/access_modifiers/Static/Course.java
@@ -0,0 +1,22 @@
+package access_modifiers.Static;
+
+public class Course {
+    public  String name;
+    public String code;
+    public int note;
+
+    public Course(String name, String code, int note) {
+        this.name = name;
+        this.code = code;
+        this.note = note;
+    }
+
+    public static void calcaverage(int[] arr){
+        double avg =0 , sum =0;
+        for (int i : arr){
+            sum +=i;
+        }
+        avg= sum/arr.length;
+        System.out.println("Average = "+avg);
+    }
+}
---
+++ src/access_modifiers/Static/Main.java
@@ -0,0 +1,17 @@
+package access_modifiers.Static;
+
+public class Main {
+    public static void main(String[] args) {
+        Course math = new Course("Math" , "math101",60);
+
+        Course eng = new Course("English" , "eng101",80);
+
+        Course physic = new Course("Pyhsic" , "phy101",40);
+
+        int[] notes ={math.note, eng.note, physic.note};
+
+        // We called the method inside Course class without creating objects
+        Course.calcaverage(notes);
+
+    }
+}
