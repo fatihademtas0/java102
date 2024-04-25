6d38ccec0c4ac208ac2da77440d89fac3e59d70b
fatihademtas0 <fatihademtas@outlook.com>
2024-04-26 01:05:46 +0300

    Encapsulation

---
+++ src/Encapsulation/Book.java
@@ -0,0 +1,33 @@
+package Encapsulation;
+
+public class Book {
+    private String name;
+    private int pageNumber;
+
+    public Book(String name, int pageNumber) {
+        this.name = name;
+        if (pageNumber <= 0) {
+            pageNumber = 0;
+        }
+        this.pageNumber = pageNumber;
+    }
+
+    public int getPageNumber() {
+        return this.pageNumber;
+    }
+
+    public void setPageNumber(int pageNumber) {
+        if (pageNumber <= 0) {
+            pageNumber = 0;
+        }
+        this.pageNumber = pageNumber;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+}
---
+++ src/Encapsulation/Main.java
@@ -0,0 +1,20 @@
+package Encapsulation;
+
+public class Main {
+    public static void main(String[] args) {
+        Book harryPotter = new Book("Harry Potter", 850);
+
+        Book lotr = new Book("Lord Of The Rings", 1300);
+
+        // harryPotter.pageNumber = 900; not working because pageNumber is private
+        // all other variables are private
+        // we can only access them with methods
+
+        harryPotter.setName("Hayri"); // changes name
+        System.out.println(harryPotter.getName());
+
+        lotr.setPageNumber(1800); // changes page number
+        System.out.println(lotr.getPageNumber());
+
+    }
+}
