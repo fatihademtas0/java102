e4a26e088688ba6a70c75c49129e029644d34969
fatihademtas0 <fatihademtas@outlook.com>
2024-05-02 17:28:48 +0300

    Encapsulation

--- src/Encapsulation03/Book.java
+++ src/Encapsulation03/Book.java
@@ -1,4 +1,4 @@
-package Encapsulation;
+package Encapsulation03;
 
 public class Book {
     private String name;
--- src/Encapsulation03/Main.java
+++ src/Encapsulation03/Main.java
@@ -1,4 +1,4 @@
-package Encapsulation;
+package Encapsulation03;
 
 public class Main {
     public static void main(String[] args) {
