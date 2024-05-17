6756e30cb1b6f2234ec810cffdbf3879c2320b4f
fatihademtas0 <fatihademtas@outlook.com>
2024-05-17 23:51:01 +0300

    Generic

---
+++ src/o07_Generic/Main.java
@@ -0,0 +1,14 @@
+package o07_Generic;
+
+public class Main {
+    public static void main(String[] args) {
+        // undefined
+        String s1;
+
+        // null
+        String s2 = null;
+
+        // empty
+        String s3= "";
+    }
+}
---
+++ src/o07_Generic/Nullable.java
@@ -0,0 +1,13 @@
+package o07_Generic;
+
+public class Nullable {
+    private final String value;
+
+    public Nullable(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+}
